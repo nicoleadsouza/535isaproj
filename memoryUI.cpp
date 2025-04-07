9d16152458084fbf1b112c9560407cfd7d217ece
Nicole D'Souza <nadsouza@umass.edu>
2025-04-07 02:03:06 -0400

    Update memoryUI.cpp

--- memoryUI.cpp
+++ memoryUI.cpp
@@ -10,11 +10,19 @@ constexpr int WORDS_PER_LINE = 4;
 constexpr int RAM_SIZE = 32768;
 constexpr int MEMORY_DELAY = 3;
 
+constexpr int STATUS_WAIT = 0;
+constexpr int STATUS_DONE = 1;
+
 struct CacheLine {
     bool valid = false;
     bool dirty = false;
     int tag = -1;
     vector<int> data = vector<int>(WORDS_PER_LINE, 0);
+};
+
+struct MemoryResult {
+    int status;
+    int value;
 };
 
 class MemorySystem {
@@ -27,7 +35,7 @@ private:
 public:
     MemorySystem() : ram(RAM_SIZE, 0), cache(CACHE_LINES) {}
 
-    void write(int address, int value, int stage) {
+    MemoryResult write(int address, int value, int stage) {
         int line_index = (address / WORDS_PER_LINE) % CACHE_LINES;
         int tag = address / (CACHE_LINES * WORDS_PER_LINE);
         int offset = address % WORDS_PER_LINE;
@@ -35,73 +43,64 @@ public:
         if (cache[line_index].valid && cache[line_index].tag == tag) {
             cache[line_index].data[offset] = value;
             cache[line_index].dirty = true;
-            cout << "done" << endl;
+            return {STATUS_DONE, 0};
         } else {
             if (cycle_count == 0) {
                 cycle_count = MEMORY_DELAY;
                 memory_access_stage = stage;
-                cout << "wait" << endl;
-            } else {
-                if (memory_access_stage == stage) {
-                    cycle_count--;
-                    if (cycle_count == 0) {
-                        ram[address] = value;
-                        cout << "done" << endl;
-                    } else {
-                        cout << "wait" << endl;
-                    }
-                } else {
-                    cout << "wait" << endl;
+                return {STATUS_WAIT, 0};
+            } else if (memory_access_stage == stage) {
+                cycle_count--;
+                if (cycle_count == 0) {
+                    ram[address] = value;
+                    return {STATUS_DONE, 0};
                 }
+                return {STATUS_WAIT, 0};
+            } else {
+                return {STATUS_WAIT, 0};
             }
         }
     }
 
-    void read(int address, int stage) {
+    MemoryResult read(int address, int stage) {
         int line_index = (address / WORDS_PER_LINE) % CACHE_LINES;
         int tag = address / (CACHE_LINES * WORDS_PER_LINE);
         int offset = address % WORDS_PER_LINE;
 
         if (cache[line_index].valid && cache[line_index].tag == tag) {
-            cout << "done " << cache[line_index].data[offset] << endl;
+            return {STATUS_DONE, cache[line_index].data[offset]};
         } else {
             if (cycle_count == 0) {
                 cycle_count = MEMORY_DELAY;
                 memory_access_stage = stage;
-                cout << "wait" << endl;
-            } else {
-                if (memory_access_stage == stage) {
-                    cycle_count--;
-                    if (cycle_count == 0) {
-                        if (cache[line_index].dirty) {
-                            // line_index = address % cache size
-                            // tag = address / cache size
-                            // so address = (tag * cache size) + line_index
-                            int oldaddr = (cache[line_index].tag * (CACHE_LINES * WORDS_PER_LINE)) + line_index;
-                            for (int i = 0; i < WORDS_PER_LINE; i++) {
-                                ram[(oldaddr / WORDS_PER_LINE) * WORDS_PER_LINE + i] = cache[line_index].data[i];
-                            }
-                        }
-                        cache[line_index].valid = true;
-                        cache[line_index].tag = tag;
-                        cache[line_index].dirty = false;
+                return {STATUS_WAIT, 0};
+            } else if (memory_access_stage == stage) {
+                cycle_count--;
+                if (cycle_count == 0) {
+                    if (cache[line_index].dirty) {
+                        int oldaddr = (cache[line_index].tag * (CACHE_LINES * WORDS_PER_LINE)) + line_index;
                         for (int i = 0; i < WORDS_PER_LINE; i++) {
-                            cache[line_index].data[i] = ram[(address / WORDS_PER_LINE) * WORDS_PER_LINE + i];
+                            ram[(oldaddr / WORDS_PER_LINE) * WORDS_PER_LINE + i] = cache[line_index].data[i];
                         }
-                        cout << "done " << cache[line_index].data[offset] << endl;
-                    } else {
-                        cout << "wait" << endl;
                     }
-                } else {
-                    cout << "wait" << endl;
+                    cache[line_index].valid = true;
+                    cache[line_index].tag = tag;
+                    cache[line_index].dirty = false;
+                    for (int i = 0; i < WORDS_PER_LINE; i++) {
+                        cache[line_index].data[i] = ram[(address / WORDS_PER_LINE) * WORDS_PER_LINE + i];
+                    }
+                    return {STATUS_DONE, cache[line_index].data[offset]};
                 }
+                return {STATUS_WAIT, 0};
+            } else {
+                return {STATUS_WAIT, 0};
             }
         }
     }
 
     void view(int level, int line) {
         if (level == 1 && line < CACHE_LINES) {
-            cout << "Cache Line " << line << " [Valid: " << cache[line].valid 
+            cout << "Cache Line " << line << " [Valid: " << cache[line].valid
                  << ", Tag: " << cache[line].tag << ", Dirty: " << cache[line].dirty << "] - ";
             for (int i : cache[line].data) cout << i << " ";
             cout << endl;
@@ -114,26 +113,19 @@ public:
             cout << "Invalid view command" << endl;
         }
     }
-};
 
-int main() {
-    MemorySystem memSys;
-    string command;
-    int address, value, level, line, stage;
+    // for testing/demoing, please leave these here until we begin to start on full demo
 
-    while (cin >> command) {
-        if (command == "W") {
-            cin >> value >> address >> stage;
-            memSys.write(address, value, stage);
-        } else if (command == "R") {
-            cin >> address >> stage;
-            memSys.read(address, stage);
-        } else if (command == "V") {
-            cin >> level >> line;
-            memSys.view(level, line);
-        } else {
-            cout << "Invalid command" << endl;
+    void forceWrite(int address, int value) {
+        if (address >= 0 && address < RAM_SIZE) {
+            ram[address] = value;
         }
     }
-    return 0;
-}
+
+    int forceRead(int address) {
+        if (address >= 0 && address < RAM_SIZE) {
+            return ram[address];
+        }
+        return 0;
+    }
+};
